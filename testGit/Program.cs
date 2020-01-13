9201f5a813abf87f4286a8885ba91010e4942006
pratheshpgp123 <pratheshcs1993@gmail.com>
2020-01-13 18:05:35 +0530

    Initial commit

---
+++ testGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git!");
+        }
+    }
+}
